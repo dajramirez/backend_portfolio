00125ebc3fe84946cd4cdfe41c73155f5d69df29
dajramirez <dariojesusramirez@gmail.com>
2024-10-09 20:25:38 +0200

    Creado el árbol de archivos

--- user-auth-system/public/index.php
+++ user-auth-system/public/index.php
@@ -14,10 +14,10 @@
       <legend>Let's log!</legend>
 
       <label for="user">Email adress</label><br>
-      <input type="email" id="user" name="user"><br>
+      <input type="email" id="user" name="user" required><br>
 
       <label for="password">Password</label><br>
-      <input type="password" id="password" name="password">
+      <input type="password" id="password" name="password" required>
 
       <button type="submit">Login</button>
 
